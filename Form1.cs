37dbe6d7c51d1f5b0409459f0974db63e80dff49
Lukasz Twardogorski <lukasz.twardogorski@gmail.com>
2018-09-04 18:27:14 +0200

    add uniform distribution

--- Form1.cs
+++ Form1.cs
@@ -12,8 +12,28 @@ namespace dewiacja
 {
     public partial class Form1 : Form
     {
+
+        //generator próbek o rozkładzie równomiernym
+        public void uniform_distribution()
+        {
+            const UInt64 m = 2147483647;
+            const UInt64 a = 16807;
+            UInt64 grain = 1179;
+            double[] table_of_sample= new double[1000];
+            for(var i=0; i<table_of_sample.Length;i++)
+            {
+                grain = (grain * a % m);
+                table_of_sample[i] = 10*grain / m;
+                Console.WriteLine(table_of_sample[i]);
+            }
+            
+            
+        }
+
+
         public Form1()
         {
+            uniform_distribution();
             InitializeComponent();
         }
     }
